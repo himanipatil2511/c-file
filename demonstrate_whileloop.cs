be05a184e6e7dcf350e092f4f784c62bbf10cb97
himanipatil2511 <98999596+himanipatil2511@users.noreply.github.com>
2022-04-28 15:03:51 +0530

    Create demonstrate_whileloop.cs

---
+++ demonstrate_whileloop.cs
@@ -0,0 +1,21 @@
+// program to demonstrate while loop in c#.net show series of given number.
+Using System;
+Namespace CSharp_Shell
+{
+    Public class Program 
+    {
+        Public static void Main()
+        {
+			Inti=1,n;
+
+			Console.WriteLine(“Enter a number:-“);
+
+		n=Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine(“seris of 1 to 10 in for loop:-“);
+		While(i<=n)
+		    {
+			Console.WriteLine(i);
+			I++;
+		    }
+		Console.ReadLine();
+        }    }}
