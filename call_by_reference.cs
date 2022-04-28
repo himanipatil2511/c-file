92d8f9cc335dbc6833a5fe13929fdafa20d6371f
himanipatil2511 <98999596+himanipatil2511@users.noreply.github.com>
2022-04-28 15:18:53 +0530

    Create call_by_reference.cs

---
+++ call_by_reference.cs
@@ -0,0 +1,17 @@
+// Program to demonstrate call by reference in c#.net.
+Using.system;
+Namespace CSharp_Shell
+{
+    Public class Program 
+{
+	Void get(ref int x)
+	{
+	X=10;
+	Console.WriteLine(x);
+	}
+        Public static void Main()
+        {			Int x=20;
+			Program p=new Program();
+			Console.WriteLine(“before function call:-“+x);
+			p.get(ref x);
+			Console.WriteLine(“after function call:-“+x);       }    }}
