f1920eb8e3e03dcdab54e0b235931372e57cee97
himanipatil2511 <98999596+himanipatil2511@users.noreply.github.com>
2022-04-28 14:50:51 +0530

    Create print_string_no_of_times.cs

---
+++ print_string_no_of_times.cs
@@ -0,0 +1,18 @@
+// Program to print “Teach one,Eachone,Tree one” for given number of times.
+using System;
+public class While
+	{
+public static void Main()
+		{
+			inti;
+			int n;
+			Console.WriteLine("Enter A Number:- ");
+			n = Convert.ToInt32(Console.ReadLine());
+			for(i=1;i<=n;i++)
+			{
+				Console.WriteLine("Teach One,EachOne,Tree One");
+				}
+			Console.ReadLine();
+			}
+	}
+
