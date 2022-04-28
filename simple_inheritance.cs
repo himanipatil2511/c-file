810d93da52341ed2c1463e00702e0ecc6518645a
himanipatil2511 <98999596+himanipatil2511@users.noreply.github.com>
2022-04-28 14:54:59 +0530

    Create simple_inheritance.cs

---
+++ simple_inheritance.cs
@@ -0,0 +1,20 @@
+//Program To demonstrate simple inheritance in c#.net.
+using System;
+Namespace CSharp_Shell
+{
+Public class Program 
+    {
+    Public	introllno=107;
+     Public string	name=”harshali”;
+    }
+    Class exam:Program
+    {
+  Public static void Main()
+        {
+	Exam n=new exam();
+Console.WriteLine(n.rollno);
+Console.WriteLine( n.name);
+	Console.ReadLine();
+        }
+    }
+}
