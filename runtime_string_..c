9e27b7bb4c2774192a9e500e445792317313cb22
himanipatil2511 <98999596+himanipatil2511@users.noreply.github.com>
2022-04-28 14:48:18 +0530

    Create runtime_string_..c

---
+++ runtime_string_..c
@@ -0,0 +1,17 @@
+Namespace using System;
+HelloWorld
+{
+class Program
+  {
+static void Main(string[] args)
+    {
+Console.WriteLine("Enter The Word ");    
+      String n1=Console.ReadLine();
+Console.WriteLine("How Many Times ");
+int no1=Convert.ToInt32(Console.ReadLine());
+for(inti=1;i<=no1;i++)
+      {
+Console.WriteLine("\n"+n1);
+      }    
+    }  }
+}
