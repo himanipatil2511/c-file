6ee2768721a088e0d9c5a312eb178cc6d63939bf
himanipatil2511 <98999596+himanipatil2511@users.noreply.github.com>
2022-04-28 15:10:34 +0530

    Create if_else.cs

---
+++ if_else.cs
@@ -0,0 +1,23 @@
+//Program to demonstrate if else statement in c#.net.
+using System;
+namespaceCSharp_Shell
+{
+public class Program 
+    {
+public static void Main()
+        {
+			int age;
+			Console.WriteLine("Entet age of candidate");
+			age=Convert.ToInt32(Console.ReadLine());
+			
+			if(age>=18)
+			{
+			Console.WriteLine("Eligible for vote");
+			}
+			else
+			{
+		Console.WriteLine("Not eligible for vote");
+			}
+        }
+    }
+}
